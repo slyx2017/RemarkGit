29e3b16ea98d09f8ca4ea0c4e54221508b3bb2ae
slyx2017 <208106500@qq.com>
2020-03-13 09:05:12 +0800

    sql表字段和说明

---
+++ 表名和说明.sql
@@ -0,0 +1,11 @@
+﻿SELECT A.name AS tablename,B.name AS columnname,C.value AS description ,d.name as datatype 
+FROM sys.tables A 
+INNER JOIN sys.columns B ON B.object_id = A.object_id 
+LEFT JOIN sys.extended_properties C ON C.major_id = B.object_id AND C.minor_id = B.column_id 
+Left join sys.types d on d.user_type_id=b.user_type_id
+
+select a.name,b.value as tabledesc 
+from sys.tables a
+left join sys.extended_properties b on b.minor_id=0 and b.major_id=a.object_id
+where a.type='U'
+order by a.name
